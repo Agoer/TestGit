1d2d4207628d04767d74b69fe2a929992e1667fc
lichan <10915819@qq.com>
2017-06-09 10:49:36 +0800

    add a file for test

---
+++ Dog.java
@@ -0,0 +1,26 @@
+ass Dog {
+	String breed;
+	int age;
+	String color;
+
+	void barking() {
+		System.out.println("barking");
+	}
+
+	void hungry() {
+		System.out.println("hungry");
+	}
+
+	void sleeping() {
+		System.out.println("sleeping");
+	}
+
+	void sleeping(String name) {
+		System.out.println("sleeping的小狗的名字是："+name);
+	}
+
+	void printAllPro() {
+		System.out.println("breed："+breed + "age:"+age+"color"+color);
+	}
+}
+
