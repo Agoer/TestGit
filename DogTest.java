d862593abc95679faeaa320be044ff1f77941937
lichan <10915819@qq.com>
2017-06-09 10:57:13 +0800

    添加了Test

---
+++ DogTest.java
@@ -0,0 +1,13 @@
+public class DogTest {
+
+public static void main (String []args) {
+	Dog dog = new Dog();
+	dog.breed = "breedString";
+	dog.age = 19;
+	dog.color = "yellow";
+	dog.printAllPro();
+
+	System.out.println("PI："+Math.PI);
+
+	}
+}	
